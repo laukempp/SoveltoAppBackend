eda9f44cd5e4160e4390fbc730da79e994f01540
Lauri Kemppainen <laukempp@gmail.com>
2020-01-14 15:50:31 +0200

    Sequelize query säätöä scores-taulun kanssa (vaiheessa)

--- soveltoapp_backend/controllers/score.js
+++ soveltoapp_backend/controllers/score.js
@@ -1,9 +1,26 @@
 const Scores = require("../models").Scores;
 const scoreservice = require("../services/score");
+const Questions = require("../models").Questions;
 
 function getScores(req, res) {
-  scoreservice.getScore().then(data => res.send(data));
+  scoreservice
+    .getScore({
+      attributes: ["id", "question", "correct_answer", "wrong_answer"],
+      include: [{ model: Scores, Questions, where: { id: 1 } }]
+      // where: { id: question_ids }
+      // include: [{ model: Scores, where: { question_ids: id } }]
+      // include: [{ model: Scores, attributes: ["nickname"] }]
+    })
+    .then(data => res.send(data));
 }
+
+// function getQuestions(req, res){
+//   topicservice.generateQuiz(
+//       {limit: req.body.number, attributes: ['id', 'question', 'correct_answer', 'wrong_answer', 'topics_id', 'q_author'],where:{topics_id: req.body.topics_id}, include:[{model: Topics, attributes: ['title']}]})
+//   .then(data => res.send(data));
+// };
+
+// select distinct question, questions.id, correct_answer, wrong_answer from questions join scores on( questions.id = any(question_ids));
 
 function getIndividualScore(req, res) {
   topicservice
