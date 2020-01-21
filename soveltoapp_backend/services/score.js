9f1aa404a8c728679ef420a918f2598d629cf538
RedEyedFrog <50462492+Oloinen@users.noreply.github.com>
2020-01-21 13:26:35 +0200

    furious mapping

--- soveltoapp_backend/services/score.js
+++ soveltoapp_backend/services/score.js
@@ -31,14 +31,30 @@ const getScore = () =>
     return data}
   );
 
+  const arrayForEach = (item, index, array) => {
+
+  }
+
+  const calculateScores = (array) => {
+    let count = 0;
+    let newArray =  array.map((item, index) => {
+      let newArray = item.user_answer.map((item, index) => {
+        count = index;
+        return ({count : item})
+      }) 
+      return newArray  
+  }) 
+  return newArray;
+}
+
   const getAllTheScores = (object) => 
   Scores.findAll(object)
   .then(score => 
     Questions.findAll({attributes: ["id", "question", "correct_answer", "wrong_answer"], where: {id: score[0].dataValues.question_ids}}) 
     .then(quizQuestions => {
     console.log(score[0].dataValues.user_answer)
-    let scoreReturn = score[0].dataValues.user_answer;
-    return {quizQuestions, score}
+    let returnScore = calculateScores(score)
+    return {quizQuestions, returnScore}
   }))
   .then(data => {
     return data}
