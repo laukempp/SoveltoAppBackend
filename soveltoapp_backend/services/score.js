751aa2557bf830b1f5e5f363df3c6915fe43e854
RedEyedFrog <50462492+Oloinen@users.noreply.github.com>
2020-01-22 13:46:32 +0200

    Array modification for scores

--- soveltoapp_backend/services/score.js
+++ soveltoapp_backend/services/score.js
@@ -31,20 +31,48 @@ const getScore = () =>
     return data}
   );
 
-  const arrayForEach = (item, index, array) => {
-
-  }
-
   const calculateScores = (array) => {
-    let count = 0;
-    let newArray =  array.map((item, index) => {
-      let newArray = item.user_answer.map((item, index) => {
-        count = index;
-        return ({count : item})
-      }) 
-      return newArray  
-  }) 
-  return newArray;
+    let fullArray = [];
+
+    let simpleArray = array.map(item => {
+      return {answer: item.user_answer}
+    })
+
+    for (let i = 0; i < simpleArray[0].answer.length; i++) {
+      let tempArray = simpleArray.map(item => {
+        return item.answer[i]
+      })
+      fullArray.push(tempArray)
+    }
+
+    /*for (let i = 0; i < array[0].user_answer.length; i++) {
+      testi.push(array[j].user_answer[j])
+      j++;
+
+    }*/
+    /*for (let i = 0; i < help[0].answer.length; i++) {
+      console.log("Ulompi looppi: " + help[i])
+      let thisArray = help.map((item, index) => {
+        return help[i][index]
+      })
+      testi.push(thisArray)
+    }
+
+    for(let i = 0; i < )*/
+
+    /*for (let e = 0; e < 2; e++) {
+      testi[e] = [];
+      for ( let i = 0; i < 3; i++) {
+       
+        testi[e][i] = help[e].answer[i];
+        }
+      }
+      let vastaus = [];
+      for (let j = 0; j < testi.length; j++) {
+        vastaus.push(testi[j][0]) 
+      }*/
+    //array[0].user_answer[0]
+    return fullArray
 }
 
   const getAllTheScores = (object) => 
@@ -54,6 +82,7 @@ const getScore = () =>
     .then(quizQuestions => {
     console.log(score[0].dataValues.user_answer)
     let returnScore = calculateScores(score)
+    let another = score[1].user_answer[0];
     return {quizQuestions, returnScore}
   }))
   .then(data => {
