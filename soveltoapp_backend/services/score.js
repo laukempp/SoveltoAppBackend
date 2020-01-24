1edb5d0a39989a1f5df373af3d1055785ecc3b21
RedEyedFrog <50462492+Oloinen@users.noreply.github.com>
2020-01-24 10:18:40 +0200

    Code cleaning

--- soveltoapp_backend/services/score.js
+++ soveltoapp_backend/services/score.js
@@ -1,78 +1,17 @@
 const Scores = require("../models").Scores;
 const Questions = require("../models").Questions;
 
-const createScore = score => Scores.create(score);
+//Apufunktiot tulosten laskemiseen
 
-const getScore = () => 
-  Scores.findAll({
-    attributes: ["question_ids", "nickname", "user_answer"]
-  })
-  .then(score => 
-    Questions.findAll({attributes: ["id", "question", "correct_answer", "wrong_answer"], where: {id: score[0].dataValues.question_ids}}) 
-    .then(quizQuestions => {
-    console.log(quizQuestions)
-    return {quizQuestions, score}
-  }))
-  .then(data => {
-    console.log(data)
-    return data}
-  );
+const modifyScoreArray = (array) => {
+  let fullArray = [];
 
-  const getOneForStudent = (object) => 
-  Scores.findAll(object)
-  .then(score => 
-    Questions.findAll({attributes: ["id", "question", "correct_answer", "wrong_answer"], where: {id: score[0].dataValues.question_ids}}) 
-    .then(quizQuestions => {
-    console.log(quizQuestions)
-    return {quizQuestions, score}
-  }))
-  .then(data => {
-    console.log(data)
-    return data}
-  );
+  let simpleArray = array.map(item => ({answer: item.user_answer}))
 
-  const modifyScoreArray = (array) => {
-    let fullArray = [];
-
-    let simpleArray = array.map(item => {
-      return {answer: item.user_answer}
-    })
-
-    for (let i = 0; i < simpleArray[0].answer.length; i++) {
-      let tempArray = simpleArray.map(item => {
-        return item.answer[i]
-      })
-      fullArray.push(tempArray)
-    }
-
-    /*for (let i = 0; i < array[0].user_answer.length; i++) {
-      testi.push(array[j].user_answer[j])
-      j++;
-
-    }*/
-    /*for (let i = 0; i < help[0].answer.length; i++) {
-      console.log("Ulompi looppi: " + help[i])
-      let thisArray = help.map((item, index) => {
-        return help[i][index]
-      })
-      testi.push(thisArray)
-    }
-
-    for(let i = 0; i < )*/
-
-    /*for (let e = 0; e < 2; e++) {
-      testi[e] = [];
-      for ( let i = 0; i < 3; i++) {
-       
-        testi[e][i] = help[e].answer[i];
-        }
-      }
-      let vastaus = [];
-      for (let j = 0; j < testi.length; j++) {
-        vastaus.push(testi[j][0]) 
-      }*/
-    //array[0].user_answer[0]
-    return fullArray
+  for (let i = 0; i < simpleArray[0].answer.length; i++) {
+    fullArray.push(simpleArray.map(item => item.answer[i]))
+  }
+  return fullArray
 }
 
 const calculateScore = (arr1, arr2) => {
@@ -93,19 +32,29 @@ const calculateScore = (arr1, arr2) => {
   return results
 }
 
+//Tietokantafunktiot
+const createScore = score => Scores.create(score);
+
+const getScore = () => 
+  Scores.findAll({
+    attributes: ["question_ids", "nickname", "user_answer"]})
+  .then(score => 
+    Questions.findAll({attributes: ["id", "question", "correct_answer", "wrong_answer"], where: {id: score[0].dataValues.question_ids}}) 
+    .then(quizQuestions => {quizQuestions, score}))
+  .then(data => data);
+
+  const getOneForStudent = (object) => 
+  Scores.findAll(object)
+  .then(score => 
+    Questions.findAll({attributes: ["id", "question", "correct_answer", "wrong_answer"], where: {id: score[0].dataValues.question_ids}}) 
+    .then(quizQuestions => calculateScore(quizQuestions, modifyScoreArray(score))))
+  .then(data => data);
+
   const getAllTheScores = (object) => 
   Scores.findAll(object)
   .then(score => 
     Questions.findAll({attributes: ["id", "question", "correct_answer", "wrong_answer"], where: {id: score[0].dataValues.question_ids}}) 
-    .then(quizQuestions => {
-    console.log(score[0].dataValues.user_answer)
-    let returnScore = modifyScoreArray(score)
-    let result = calculateScore(quizQuestions, returnScore);
-    let another = score[1].user_answer[0];
-    return result
-  }))
-  .then(data => {
-    return data}
-  );
+    .then(quizQuestions => calculateScore(quizQuestions, modifyScoreArray(score))))
+  .then(data =>  data);
 
 module.exports = { createScore, getScore, getOneForStudent, getAllTheScores };
