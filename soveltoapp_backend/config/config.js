b5a9159e606b2ad24004e03bb4f3650f04e21061
Tommi Teräsvirta <tommi.terasvirta@sovelto.fi>
2020-02-26 09:24:57 +0200

    Ympäristömuuttujat ja configit hieman toisin

--- soveltoapp_backend/config/config.js
+++ soveltoapp_backend/config/config.js
@@ -3,6 +3,18 @@ module.exports = {
     port: 5432,
     saltRounds: 3,
     jwtSecret: 'yo-its-a-secret',
-    tokenExpireTime: '6h'
+    tokenExpireTime: '6h',
+    sequelizeConfig: {
+        host: 'localhost',
+        dialect: 'postgres',
+        port: 5432,
+    
+        pool: {
+            max: 9,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
 }
 
